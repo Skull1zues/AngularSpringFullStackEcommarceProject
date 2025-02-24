091b18d8f4a268038dac8d345b0a82e730e1625e
Soumyadip Barik <soumyadip.barik@vodafone.com>
2025-02-24 13:50:49 +0530

    3. Define Router outlet 4. setup router links to pass category id 5. Enhence productListComponent to read categoryId 6. Modify Springboot Dao class to get ProductByCategory 7. Update angular service to call new URL on spring boot app

--- FrontEnd/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ FrontEnd/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,25 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
 import { Product } from 'src/app/common/product';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list-grid.comonent.html',
-  // templateUrl: './product-list.component.html',
+ 
+
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
+  currentCategoryId: number | undefined;
   
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService, 
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.listProducts();
+    this.route.paramMap.subscribe(()=> {
+      this.listProducts();
+    });
   }
 
   listProducts() {
-    this.productService.getProductList().subscribe(
+    // check if "id" parameter is available
+    const hasCategoryId: boolean =this.route.snapshot.paramMap.has('id');
+    const id = this.route.snapshot.paramMap.get('id')
+
+    if (id) {
+      
+      // get the "id" param string. convert string to a number using "+" symbol
+      this.currentCategoryId = +id
+    }
+    else{
+      // not catagory id available .. default category id is 1
+      this.currentCategoryId = 1;
+    }
+
+
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }
