9d2f127e646b0d00fffe80d49e93745c57b13ec5
Soumyadip Barik <soumyadip.barik@vodafone.com>
2025-03-30 14:10:28 +0530

    Add pagination(3. update ProductListComponent to handle pagination, 4. Enhance html support)

--- FrontEnd/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ FrontEnd/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -14,7 +14,15 @@ export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
   currentCategoryId: number | undefined;
+  previousCategoryId: number =1;
   searchMode: boolean =false;
+
+  // new component for pagination
+  thePageNumber: number = 1;
+  thePageSize: number = 10;
+  theTotalElements: number =0;
+  
+
   
   constructor(private productService: ProductService, 
               private route: ActivatedRoute) { }
@@ -63,10 +71,27 @@ export class ProductListComponent implements OnInit {
       this.currentCategoryId = 1;
     }
 
+    //
+    // check if we have a diffent categoryId
+    // Note: angular will reuse the component if it is being viewed
+    //
 
-    this.productService.getProductList(this.currentCategoryId).subscribe(
+    if(this.previousCategoryId!= this.currentCategoryId){
+      this.thePageNumber = 1;
+    }
+    this.previousCategoryId =this.currentCategoryId;
+
+    console.log(`currentCategoryId= ${this.currentCategoryId}, pageNumber = ${this.thePageNumber}`)
+
+
+    this.productService.getProductListPaginate(this.thePageNumber-1,
+                                      this.thePageSize,
+                                      this.currentCategoryId).subscribe(
       data => {
-        this.products = data;
+        this.products = data._embedded.products;
+        this.thePageNumber = data.page.number +1;
+        this,this.thePageSize = data.page.size;
+        this.theTotalElements = data.page.totalElements;
       }
     )
 
