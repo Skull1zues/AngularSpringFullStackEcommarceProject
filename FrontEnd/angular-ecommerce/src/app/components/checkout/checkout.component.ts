cc0004d4a7e1d0671d33b2e852ca139be96991b8
Soumyadip Barik <soumyadip.barik@vodafone.com>
2025-03-31 19:36:42 +0530

    Add ShippingDetails, BillingDetails and CreditCarddetails

--- FrontEnd/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ FrontEnd/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -8,7 +8,11 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class CheckoutComponent implements OnInit {
 
+
   checkoutFormGroup!: FormGroup;
+
+  totalPrice: number=0;
+  totalQuantity: number = 0;
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
@@ -17,10 +21,42 @@ export class CheckoutComponent implements OnInit {
         firstName: [''],
         lastName: [''],
         email: ['']
+      }),
+      shippingAddress : this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country:[''],
+        pincode: ['']
+      }),
+      billingAddress : this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country:[''],
+        pincode: ['']
+      }),
+      creditCard : this.formBuilder.group({
+        cardType: [''],
+        nameOnCard: [''],
+        cardNumber: [''],
+        cvv: [''],
+        enpirationMonth:[''],
+        enpirationYear:['']
       })
 
     })
   }
+  copyShippingAddressToBillingAddress(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    console.log('Checkbox state:', inputElement.checked);
+    if (inputElement.checked){
+      this.checkoutFormGroup.controls['billingAddress'].
+      setValue(this.checkoutFormGroup.controls['shippingAddress'].value)
+    }else{
+      this.checkoutFormGroup.controls['billingAddress'].reset();
+    }
+    }
 
   onSubmit(){
     console.log("Handling the submit button");
