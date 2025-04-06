dfba7889481639af5d56742fedc6489e16786681
Soumyadip Barik <soumyadip.barik@vodafone.com>
2025-04-06 13:19:04 +0530

    ANgular Okta Intregation - (1. setUp app config for openId connect 2.Installed OKTA sdk dependency 3. Intregate OKTA signin Widgets)

---
+++ FrontEnd/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default  {
+    oidc: {
+        clientId: 'smvT9GzI8njg1JNsaHulwptLu9rmp2e4',
+        issuer: 'https://dev-e8vigvs7hi8lgvvj.us.auth0.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid' , 'profile', 'email']
+    }
+}
